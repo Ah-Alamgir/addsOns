71e46da88a94281709259df0e7a183ab22a8c241
ahalamgir16 <ahalamgir16@outlook.com>
2023-05-23 09:15:46 +0600

    done

---
+++ app/src/main/java/com/waskuroni/addons/ReferCOde.java
@@ -0,0 +1,14 @@
+package com.waskuroni.addons;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReferCOde extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_refer_code);
+    }
+}
